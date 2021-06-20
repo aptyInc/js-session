7253b806d4275a181ae405c5e7193f5b658a0bad
AnirudhSinghTomar <anirudhtomar0912@gmail.com>
2021-06-20 23:16:57 +0530

    Callback example

---
+++ 01-javascript/03-callback/anirudhsinghtomar.js
@@ -0,0 +1,54 @@
+//Simple factorial function using for loop
+function factorial(number)
+{
+  if (number === 0 || number === 1)
+    return 1;
+
+
+  for (let i = number - 1; i >= 1; i--)
+  {
+
+    number = number * i;
+
+  }
+   return number;
+}
+
+
+//callback function example
+//A function goes into Web Api if it satisty any of the two conditions: Callback or promises.
+//Since it is javascript we can have another function as an arguement of a function.
+function factor(number , callback) {
+  setTimeout( () => {
+   //In case you input a string as an input then the output cannot be calculated
+    if(typeof number !== "number")
+    {
+      err = true;
+      // callback is a error first function, since first arguments is error and second is data
+      callback(err,null);
+      return;
+    }
+
+  const result = factorial(number);
+  // callback is a error first function, since first arguments is error and second is data
+  callback(null , result);
+
+},1000);
+}
+
+
+let callback = (err , result) =>
+{
+  if(err !== null)
+  {
+    console.log("Error is caught" );
+    return;
+  }
+  console.log(result);
+
+}
+
+factor(5,callback);
+//Output: 120
+factor("Anirudh",callback);
+//Output : Error is caught
