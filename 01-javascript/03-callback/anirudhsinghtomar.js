b5ec45eb93a0ef8d937e8050deaff96ec231fe67
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-06-21 15:33:06 +0530

    Update anirudhsinghtomar.js

--- 01-javascript/03-callback/anirudhsinghtomar.js
+++ 01-javascript/03-callback/anirudhsinghtomar.js
@@ -1,60 +1,74 @@
-//Simple factorial function using for loop
 function factorial(number)
 {
   if (number === 0 || number === 1)
     return 1;
-
-
   for (let i = number - 1; i >= 1; i--)
   {
-
     number = number * i;
-
   }
    return number;
 }
+
+/*
+//Stack
+
+function factor(number)
+{ //Web Api
+  setTimeout( () => {
+    if(typeof number !== "number" || number<0)
+  {
+    console.log("Error is caught");
+  }  
+  return (factorial(number));
+  
+  } ,1000);
+}
+
+console.log(factor(5));
+Output:
+
+undefined 
+
+We will be solving this problem using callback
+*/
 
 
 //callback function example
 //A function goes into Web Api if it satisty any of the two conditions: Callback or promises.
 //Since it is javascript we can have another function as an arguement of a function.
+//Stack
 function factor(number , callback) {
-  setTimeout( () => {
-   //In case you input a string as an input then the output cannot be calculated
-    if(typeof number !== "number")
+  //Web Api
+  setTimeout( (err,data) => {
+    if(typeof number !== "number" || number<0)
     {
-      err = true;
-      // callback is a error first function, since first arguments is error and second is data
-      callback(err,null);
-      return;
+      err=true;
     }
-    if(number<0)
-    {
-      err = true;
-      // callback is a error first function, since first arguments is error and second is data
-      callback(err,null);
-      return;
-    }  
-
-  const result = factorial(number);
-  // callback is a error first function, since first arguments is error and second is data
-  callback(null , result);
-
+     
+     if(err)
+     {
+       return callback(err,null);
+     }
+  
+  data = factorial(number);
+  return callback(null , data);
 },1000);
+//The function would not have returned anything if there was no callback function
 }
 
+datausage=(data)=>console.log(data);
+errorhandling=(err)=>console.log("Error is caught");
 
-let callback = (err , result) =>
+let callback = (err , data) =>
 {
   if(err !== null)
-  {
-    console.log("Error is caught" );
-    return;
-  }
-  console.log(result);
-
+  errorhandling(err);
+  
+  else
+  datausage(data);
+  
+  return;
 }
-
 factor(5,callback);
 //Output: 120
 factor("Anirudh",callback);
