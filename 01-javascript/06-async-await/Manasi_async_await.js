d61107ac886a7493f092eb02ac05ae9f2196da4c
manasi-04 <84763834+manasi-04@users.noreply.github.com>
2021-06-24 13:21:49 +0530

    Update async-await.js
    Formatted code

--- 01-javascript/06-async-await/Manasi_async_await.js
+++ 01-javascript/06-async-await/Manasi_async_await.js
@@ -4,33 +4,39 @@ const { readFile } = require('fs');
 var lolfound = false;
 var sasafound = false;
 
+
 //function returns a promise
 function fileReaderPromise(path) {
     return new Promise((res,rej)=>{
         readFile(path, { encoding: 'utf-8' }, (err, data) => {
                 return err ? rej(err) : res(data);
             
-    })
+        })
     })
 }
+
 
 //try catch block is used to catch any errors while reading files
 async function readFiles(){
     try{
-        const loldata = await fileReaderPromise('./lol.txt');
-        if(loldata)
-        lolfound = true;
-        const sasadata = await fileReaderPromise('./sasas.txt');
-        if(sasadata)
-        sasafound = true;
-        console.log({loldata,sasadata});
-    }catch (error){
-        if(!lolfound)
-        console.log("File1 caused error");
-        else if(!sasafound)
-        console.log("File2 caused error")
-        console.error(error);
+            const loldata = await fileReaderPromise('./lol.txt');
+            if(loldata)
+                lolfound = true;
+
+            const sasadata = await fileReaderPromise('./sasa.txt');
+            if(sasadata)
+                sasafound = true;
+
+            console.log({loldata,sasadata});
+      }catch (error){
+            if(!lolfound)
+                console.log("File1 caused error");
+            else if(!sasafound)
+                console.log("File2 caused error");
+
+            console.error(error);
     }
 }
 
+//Calling the readFiles function
 readFiles();
